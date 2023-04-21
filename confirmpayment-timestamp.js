e256a104d29fdc4a68f408656bc325635c416b3f
faizkung <voranuch.nanta@gmail.com>
2023-04-21 15:39:42 +0700

    add all file

---
+++ confirmpayment-timestamp.js
@@ -0,0 +1,10 @@
+function (request, response, logger) {
+    let time = new Date().toString();
+    return {
+        "statusCode": 200,
+        "body" : {
+            "notify_message": "วันเวลาที่ชำระเงิน 1/3/2020 13:30:00 หมายเลขคำสั่งซื้อ 8004359122 คุณสามารถติดตามสินค้าผ่านช่องทาง Kerry หมายเลข 1785261900",
+            "timestamp": time
+        }
+    };
+}
